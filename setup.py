18859f5285e10b1b2c0247a8cb04ada1b700fcc5
Rod Glover <rglover@uvic.ca>
2017-08-17 17:40:18 -0700

    Add requirements.txt and setup.py

---
+++ setup.py
@@ -0,0 +1,44 @@
+from setuptools import setup
+
+__version__ = (0, 1, 0)
+
+setup(
+    name='dp',
+    description='PCIC Climate Explorer Data Preparation',
+    version='.'.join(str(d) for d in __version__),
+    author='Rod Glover',
+    author_email='rglover@uvic.ca',
+    url='https://github.com/pacificclimate/climate-explorer-data-prep',
+    keywords='science climate meteorology downscaling modelling climatology',
+    zip_safe=True,
+    install_requires='''
+        python-dateutil
+        cdo
+        nchelpers
+        pint
+        PyYAML
+    '''.split(),
+    package_data = {
+        'dp': ['tests/data/*.nc']
+    },
+    scripts='''
+        scripts/generate_climos
+        scripts/split_merged_climos
+        scripts/update_metadata
+    '''.split(),
+    classifiers='''
+        Development Status :: 4 - Beta,
+        Environment :: Console,
+        Intended Audience :: Developers,
+        Intended Audience :: Science/Research,
+        License :: OSI Approved :: GNU General Public License v3 (GPLv3)
+        Operating System :: OS Independent,
+        Programming Language :: Python :: 2.7,
+        Programming Language :: Python :: 3.5,
+        Programming Language :: Python :: 3.6,
+        Topic :: Scientific/Engineering,
+        Topic :: Database,
+        Topic :: Software Development :: Libraries :: Python Modules
+    '''.split()
+
+)
