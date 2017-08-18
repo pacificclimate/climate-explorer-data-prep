52fffd3ce8d660b8c9b98b84d072efa861a034a4
Rod Glover <rglover@uvic.ca>
2017-08-17 17:28:03 -0700

    Fix conftest

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,12 +1,11 @@
 from pytest import fixture, mark
 from pkg_resources import resource_filename
 from nchelpers import CFDataset
-from dp.generate_climos import create_climo_files
 
 
 @fixture
 def tiny_dataset(request):
-    return CFDataset(resource_filename('dp', 'tests/data/tiny_{}.nc').format(request.param))
+    return CFDataset(resource_filename(__name__, 'data/tiny_{}.nc').format(request.param))
 
 
 @fixture(scope='function')
