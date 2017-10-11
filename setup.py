f445d88dba2c9a15a792e8c0f70a18e929c5e2db
Rod Glover <rglover@uvic.ca>
2017-10-11 10:01:45 -0700

    Add data file containing variable info

--- setup.py
+++ setup.py
@@ -20,8 +20,12 @@ setup(
     '''.split(),
     packages=['dp'],
     package_data = {
-        'dp': ['tests/data/*.nc']
+        'dp': [
+            'dp/variable information.csv',
+            'tests/data/*.nc'
+        ]
     },
+    include_package_data=True,
     scripts='''
         scripts/generate_climos
         scripts/split_merged_climos
