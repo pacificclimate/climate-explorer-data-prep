dd1c9832aba91100d98579617511dd2543f7e1a0
Nikola Rados <nrados95@gmail.com>
2019-03-19 14:49:02 -0700

    Update logic and clean up code

--- dp/generate_prsn.py
+++ dp/generate_prsn.py
@@ -3,7 +3,10 @@ import numpy as np
 import os
 
 from statistics import mean
+from math import floor
 from netCDF4 import Dataset
+
+from dp.units_helpers import Unit
 
 
 # Set up logging
@@ -21,18 +24,27 @@ def in_shape(arrays):
     return len(set(map(np.shape, arrays))) == 1
 
 
-def build_prsn_array(pr_data, means, units):
-    '''Mask precipitation data where the temperature is not below freezing'''
-    if len(units) > 1:
-        raise Exception('Temperature files units do not match: {}'.format(units))
+def is_unique_value(values):
+    '''
+    Given a list transform into set and ensure there is only one unique value
+    '''
+    return len(set(values)) == 1
 
-    freezing = 0
-    if units.pop() == 'K':
+
+def determine_freezing(unit):
+    '''Given a unit determine which temperature describes freezing'''
+    freezing = 0.0
+    if unit == 'K':
         freezing = 273.15
+    return freezing
+
+
+def build_prsn_array(pr_data, means, freezing):
+    '''Mask precipitation data where the temperature is not below freezing'''
     return np.where(means < freezing, pr_data, np.nan)
 
 
-def create_netcdf_from_source(input_filepath, output_filepath):
+def create_prsn_netcdf_from_source(input_filepath, output_filepath):
     '''Using another netCDF as a template copy over the data applicable to prsn'''
     with Dataset(input_filepath) as src, Dataset(output_filepath, mode='w') as dst:
         # Create the dimensions of the file
@@ -74,50 +86,92 @@ def copy_netcdf_data(output_filepath, data, start, end):
         dst.variables['prsn'][start:end] = data
 
 
-def create_output_filepath(filepath, outdir):
+def create_filepath_from_source(source_path, new_var, outdir):
     '''
-    Using the precipitation filename and output directory build an output
-    filepath
+    Using the source filename build a new output filepath with new_var and
+    output directory.
     '''
-    file_name = filepath.split('/')[-1]
+    file_name = source_path.split('/')[-1]
     variable, *rest = file_name.split('_')
-    variable = 'prsn'
+
     suffix = ''
     for var in rest:
         suffix += '_' + var
+
     if not os.path.exists(os.path.dirname(outdir)):
         os.makedirs(os.path.dirname(outdir))
-    return os.path.join(outdir, variable + suffix)
+
+    return os.path.join(outdir, new_var + suffix)
 
 
-def match_datasets(datasets):
-    '''Determine whether the files will produce a correct result'''
+def has_required_vars(datasets, required_vars):
+    '''
+    Given a list of datasets and a list of required variables, ensure the
+    datasets contain all required variables.
+    '''
     unique_vars = set()
-    experiment_id = set()
-    model_id = set()
-    parent_experiment_rip = set()
-
     for dataset in datasets:
         for var in dataset.variables:
             unique_vars.add(var)
 
-        experiment_id.add(dataset.getncattr('experiment_id'))
-        model_id.add(dataset.getncattr('model_id'))
-        parent_experiment_rip.add(dataset.getncattr('parent_experiment_rip'))
-
-    # ensure all match
-    if len(experiment_id) != 1 or len(model_id) != 1 or len(parent_experiment_rip) != 1:
-        return False
-
-    # ensure we have the required variables to produce prsn
-    for required_var in ['pr', 'tasmin', 'tasmax']:
+    for required_var in required_vars:
         if required_var not in unique_vars:
+            logger.warning('Files do not contain required variables. required: {} have: {}'
+                           .format(required_vars, unique_vars))
             return False
 
     return True
 
 
-def chunk_process_netcdf(pr, tasmin, tasmax, max_len, output_filepath, chunk_size=100):
+def matching_datasets(datasets):
+    '''
+    Given a list of datasets, match important metadata vars to ensure the
+    datasets are compatible.
+    '''
+    experiment_id = []
+    model_id = []
+    parent_experiment_rip = []
+
+    for dataset in datasets:
+        experiment_id.append(dataset.getncattr('experiment_id'))
+        model_id.append(dataset.getncattr('model_id'))
+        parent_experiment_rip.append(dataset.getncattr('parent_experiment_rip'))
+
+    # ensure metadata match
+    if not is_unique_value(experiment_id) or not is_unique_value(model_id) \
+       and not is_unique_value(parent_experiment_rip):
+        logger.warning(('Files do not have the same metadata:'
+                        '\n\texperiment_id: {}'
+                        '\n\tmodel_id: {}'
+                        '\n\tparent_experiment_rip: {}')
+                       .format(experiment_id, model_id, parent_experiment_rip))
+        return False
+
+    return True
+
+
+def matching_temperature_units(tasmin, tasmax):
+    '''Ensure the temperature datasets have matching units'''
+    return is_unique_value([tasmin.variables['tasmin'].units,
+                            tasmax.variables['tasmax'].units])
+
+
+def check_pr_units(pr):
+    '''Ensure we have expected pr units'''
+    pr_variable = pr.variables['pr']
+    pr_units = Unit.from_udunits_str(pr_variable.units)
+    return pr_units in [Unit('kg / m**2 / s'), Unit('mm / s')]
+
+
+def preprocess_checks(pr, tasmin, tasmax, required_vars):
+    '''Perform all pre-processing checks'''
+    return matching_datasets([pr, tasmin, tasmax]) and \
+        has_required_vars([pr, tasmin, tasmax], required_vars) and \
+        matching_temperature_units(tasmin, tasmax) and \
+        check_pr_units(pr)
+
+
+def process_to_prsn(pr, tasmin, tasmax, max_len, output_filepath, freezing):
     '''Process precipitation data into snowfall data
 
     This method takes a precipitation, tasmin and tasmax file and uses them to
@@ -127,7 +181,8 @@ def chunk_process_netcdf(pr, tasmin, tasmax, max_len, output_filepath, chunk_siz
     are left with cells where it was cold enough for snow.
 
     The input files are read in small chunks to avoid filling up all available
-    memory and crashing the script.
+    memory and crashing the script.  Size refers to the chunk size being read
+    in from the time dimension.  Example: Chunk read in (100, all lon, all lat)
 
     Parameters:
         pr (netCDF.Dataset): Dataset object for precipitation
@@ -137,26 +192,25 @@ def chunk_process_netcdf(pr, tasmin, tasmax, max_len, output_filepath, chunk_siz
             the loop condition
         output_filepath (str): Path to base directory in which to store output
             prsn file
-        chunk_size (int): Size of time dimension to be read from the datacube
-            (Defaulted to 100). The chunk of datacube being accessed will look
-            like: (chunk_size, lat_size, lon_size).
+        freezing (float): Freezing temperature
     '''
     # TODO: Figure out a way to dynamically choose chunksize
-    chunk_start = 0
-    chunk_end = chunk_size
+    size = 100
+    start = 0
+    end = size
 
-    if chunk_size > max_len:
-        chunk_size = max_len
+    if size > max_len:
+        size = max_len
 
-    while(chunk_start < max_len):
+    while(start < max_len):
         # end of array check
-        if chunk_end > max_len:
-            chunk_end = max_len
+        if end > max_len:
+            end = max_len
 
         # get variable data
-        pr_data = pr.variables['pr'][chunk_start:chunk_end]
-        tasmin_data = tasmin.variables['tasmin'][chunk_start:chunk_end]
-        tasmax_data = tasmax.variables['tasmax'][chunk_start:chunk_end]
+        pr_data = pr.variables['pr'][start:end]
+        tasmin_data = tasmin.variables['tasmin'][start:end]
+        tasmax_data = tasmax.variables['tasmax'][start:end]
 
         # shape check
         if not in_shape([pr_data, tasmin_data, tasmax_data]):
@@ -166,17 +220,15 @@ def chunk_process_netcdf(pr, tasmin, tasmax, max_len, output_filepath, chunk_siz
         means = np.mean([tasmin_data, tasmax_data], axis=0)
 
         # build data
-        units = {tasmin.variables['tasmin'].units,
-                 tasmax.variables['tasmax'].units}
-        prsn_data = build_prsn_array(pr_data, means, units)
+        prsn_data = build_prsn_array(pr_data, means, freezing)
 
         # write netcdf
         logger.debug('Write prsn data to netCDF')
-        copy_netcdf_data(output_filepath, prsn_data, chunk_start, chunk_end)
+        copy_netcdf_data(output_filepath, prsn_data, start, end)
 
         # prep next loop
-        chunk_start = chunk_end
-        chunk_end += chunk_size
+        start = end
+        end += size
 
 
 def generate_prsn_file(pr_filepath, tasmin_filepath, tasmax_filepath, outdir):
@@ -196,16 +248,18 @@ def generate_prsn_file(pr_filepath, tasmin_filepath, tasmax_filepath, outdir):
     tasmax = Dataset(tasmax_filepath)
 
     # make sure datasets match
-    if not match_datasets([pr, tasmin, tasmax]):
-        raise Exception('Datasets do not match, please ensure you are using the correct files')
-    logger.info('Dataset are matching')
+    logger.info('Conducting pre-process checks')
+    if not preprocess_checks(pr, tasmin, tasmax, ['pr', 'tasmin', 'tasmax']):
+        raise Exception('Pre-process checks have failed.')
 
     # create template nc file from pr file
     logger.info('Creating outfile')
-    output_filepath = create_output_filepath(pr_filepath, outdir)
-    create_netcdf_from_source(pr_filepath, output_filepath)
+    output_filepath = create_filepath_from_source(pr_filepath, 'prsn', outdir)
+    create_prsn_netcdf_from_source(pr_filepath, output_filepath)
 
     logger.info('Processing files in chunks')
-    chunk_process_netcdf(pr, tasmin, tasmax, len(pr.variables['pr']), output_filepath)
+    freezing = determine_freezing(tasmin.variables['tasmin'].units)
+    max_len = len(pr.variables['pr'])
+    process_to_prsn(pr, tasmin, tasmax, max_len, output_filepath, freezing)
 
     logger.info('Complete')
